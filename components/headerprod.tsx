4d6386fa08a8fabce322da5af3df511feec480ca
raffamartyn <raffamartyn@gmail.com>
2023-03-29 09:18:27 -0300

    listo

--- components/headerprod.tsx
+++ components/headerprod.tsx
@@ -4,13 +4,14 @@ import { Stack,Image, Text } from "@chakra-ui/react";
 
 const Headerproduct = () => {
     return(
-        <Stack pb={['0','20']} height={['100%','90vh']} width={'100%'}>
+        <Stack pb={['0','20']} height='100%' width='100%'>
            
             <Image
     src='./hederpro.gif'
     height={['50%','110%']}
     alt='Dan Abramov'
-  /> <Text display={'flex'} pt={['190px','470px']} position={'absolute'}>hola soy el texto </Text>
+  /> 
+  <Text display={'flex'} pt={['190px','470px']} position={'absolute'}>hola soy el texto </Text>
 
         </Stack>
     )
